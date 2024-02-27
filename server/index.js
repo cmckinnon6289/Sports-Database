5472f1d25cc3ad6e7745da8ad52ec800f24179e3
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-02-27 11:24:28 -0500

    added some API endpoints

--- server/index.js
+++ server/index.js
@@ -16,3 +16,30 @@ app.listen(PORT, () => {
 })
 
 require('./db');
+
+api.get('/api/internal/test', async(req, res) => {
+    try {
+        res.json({ message: "hi! the API is working. have a great day :)" });
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+app.get('/api/assets/all-events', async(req, res) => {
+    try {
+        const events = await Event.find({});
+        res.json(events);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+app.post('/api/new-event', async (req, res) => {
+    try {
+        const event = new Event(req.body);
+        await event.save();
+        res.json(event);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
