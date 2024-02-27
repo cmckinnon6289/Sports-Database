55686e18004e28343ead1d875120bec72f1442cb
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-02-27 10:56:28 -0500

    process.env.completed sunglasses

--- server/db.js
+++ server/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-const connectionString = '';
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.adsptvs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=sports-database`;
