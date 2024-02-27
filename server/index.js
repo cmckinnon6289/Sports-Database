87e3136bf15d241f8f59b2235080c624a625be0f
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-02-27 11:06:14 -0500

    idk

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const Event = require('./models/Event');
+
 const app = express();
 const PORT = process.env.PORT || 621;
 
@@ -12,3 +14,5 @@ app.use(bodyParser.json());
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 })
+
+require('./db');
