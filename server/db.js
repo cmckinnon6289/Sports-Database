b0803105fe73edf1d488621f9750da9cc61aa6c9
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-02-27 11:05:27 -0500

    new tings

--- server/db.js
+++ server/db.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 
 const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.adsptvs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=sports-database`;
+
+mongoose.connect(connectionString, {}).then(() => console.log('Connected to MongoDB.')).catch((err) => console.log(err));
---
+++ server/models/Event.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    homeTeam: {
+        type: Object,
+        required: true
+    },
+    awayTeam: {
+        type: Object,
+        required: true
+    },
+    timing: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    notes: String
+})
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
