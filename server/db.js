9e629e97854a8bde1e38cc8dd6059c25e1b433cd
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-04-05 11:10:41 -0400

    commented enviro, seems to be causing mongo errors on render

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const enviro = require('dotenv')
-enviro.config()
+// const enviro = require('dotenv')
+// enviro.config()
 
 const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.adsptvs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=sports-database`;
 
