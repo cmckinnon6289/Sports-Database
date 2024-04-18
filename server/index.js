aeb23d68dd82a4532eb24659ecc0ae0e0723476f
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-04-18 13:23:02 -0400

    perms returns perms instead of user

--- server/index.js
+++ server/index.js
@@ -162,7 +162,8 @@ app.get('/api/users/get-perms/:id', async(req, res) => {
         console.log(req.params.id);
         const user = await User.findById(req.params.id);
         if (!user) return res.status(404).json({error: `could not find user with id ${req.body.id}.`});
-        res.status(200).json(user);
+        res.status(200).json(user.perms);
+        // :0
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
